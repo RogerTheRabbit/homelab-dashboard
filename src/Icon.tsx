567f5d7656d12934aff64f0b59537b0d25b424f9
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2025-05-25 21:45:47 -0400

    Add unavailable image when missing

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -4,12 +4,18 @@ export type Props = {
 };
 
 function Icon(props: Props) {
+  const handleError = (e: any) => {
+    e.target.src = "homelab-dashboard/unavailable.svg";
+  };
+
   return (
     <a href={props.href}>
-      <div
-        className="bg-zinc-800 hover:bg-zinc-700 rounded-3xl p-5 m-2.5 transition-colors duration-200 ease-in"
-      >
-        <img className="min-w-24 w-24 min-h-24 h-24" src={props.img} />
+      <div className="bg-zinc-800 hover:bg-zinc-700 rounded-3xl p-5 m-2.5 transition-colors duration-200 ease-in">
+        <img
+          className="min-w-24 w-24 min-h-24 h-24"
+          src={props.img}
+          onError={handleError}
+        />
       </div>
     </a>
   );
